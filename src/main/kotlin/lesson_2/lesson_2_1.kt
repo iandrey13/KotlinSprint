3439e3e62241c8d3a2406beec53da3b42e1e5499
iandrey <iandrey@>
2024-08-26 22:20:21 +0400

    KS-2-1 do arithmetic mean

---
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main () {
+    val sumOfRatings = 3 + 4 + 3 + 5
+    var arithmeticMean : Float = 0f
+    arithmeticMean = sumOfRatings / 4f
+println(arithmeticMean)
+
+
+}
