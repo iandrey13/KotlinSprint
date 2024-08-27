83c1d793166adffe842fa94c945b0535be9438ee
iandrey <iandrey@>
2024-08-27 20:02:47 +0400

    KS-2-1 do corrections

--- src/main/kotlin/lesson_2/lesson_2_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_2
 
-fun main () {
-    val sumOfRatings = 3 + 4 + 3 + 5
-    var arithmeticMean : Float = 0f
-    arithmeticMean = sumOfRatings / 4f
-println(arithmeticMean)
+fun main() {
+    val sumOfRatings: Int
+    var arithmeticmean: Float
+
+    sumOfRatings = 3 + 4 + 3 + 5
+    arithmeticmean = sumOfRatings / 4f
+    println(arithmeticmean)
 
 
 }
